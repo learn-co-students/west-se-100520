ef39ecd83d58f117e2e37db53619ef551dbacebb
github username <github email address>
2020-10-09 10:16:33 -0700

    before lecture 5

---
+++ 5-theatre-code-challenge-review/config/environment.rb
@@ -0,0 +1,5 @@
+require 'bundler/setup'
+require 'date'
+
+Bundler.require
+require_all 'app'
---
+++ 5-theatre-code-challenge-review/tools/console.rb
@@ -0,0 +1,9 @@
+require_relative '../config/environment.rb'
+
+def reload
+  load 'config/environment.rb'
+end
+
+# your seed data goes here
+
+Pry.start
