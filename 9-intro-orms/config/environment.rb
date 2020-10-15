2149299e52becb64cb8ecf84df51659e27493276
github username <github email address>
2020-10-15 14:10:05 -0700

    mid lecture 9

--- 9-intro-orms/config/environment.rb
+++ 9-intro-orms/config/environment.rb
@@ -1,5 +1,6 @@
 require 'bundler'
 Bundler.require
+# require 'pry'
 
 DB = {
   conn: SQLite3::Database.new('db/twitter.db')
@@ -13,7 +14,14 @@ DB = {
 #     end
 #   end
 # end
-
+sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS tweets (
+        id INTEGER PRIMARY KEY,
+        username TEXT,
+        message TEXT
+        )
+    SQL
+DB[:conn].execute(sql)   
 DB[:conn].results_as_hash = true
 
 require_relative '../lib/tweet.rb'
--- 9-intro-orms/lib/tweets_app.rb
+++ 9-intro-orms/lib/tweets_app.rb
@@ -6,22 +6,42 @@ class TweetsApp
   end
   
   def menu
+    loop do
+      display_menu_options
+      choice = gets.strip
+      case choice
+      when "n"
+        puts 'Enter a username:'
+        username = gets.chomp
+        
+        puts 'Enter a message:'
+        message = gets.chomp
+        
+        tweet = Tweet.new({'username' => username, 'message' => message})
+        
+        tweets = Tweet.all
+        render(tweets)
+      when "ls"
+        render(Tweet.all)
+      when "e"
+        exit
+      else
+        puts "I don't understand that choice"
+      end
+    end
     
-    puts 'Enter a username:'
-    username = gets.chomp
-    
-    puts 'Enter a message:'
-    message = gets.chomp
-    
-    tweet = Tweet.new({'username' => username, 'message' => message})
-    
-    tweets = Tweet.all
-    render(tweets)
-    menu
+  end
+
+  def display_menu_options
+    puts
+    puts "What do you want to do?"
+    puts "-----------------------"
+    puts "[n] Make new tweet"
+    puts "[ls] Display all tweets"
+    puts "[e] To exit program"
   end
   
   def intro
-    
     puts 'Welcome to Twitter'
   end
 
@@ -29,7 +49,7 @@ class TweetsApp
 
   def render(tweets)
     tweets.each.with_index(1) do |tweet, i|
-      puts "#{i}. #{tweet.username} says: #{tweet.message}"
+      puts "#{i}. #{tweet.username} says: #{tweet.message} (id: #{tweet.id}"
     end
   end
 end
