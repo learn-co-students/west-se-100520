cb6fd70942c82a17b26d9985ae6700dcbf9a3e4a
github username <github email address>
2020-11-17 10:15:30 -0800

    before lecture 34

--- 34-events-intro/bubbling/bubbling.js
+++ 34-events-intro/bubbling/bubbling.js
@@ -1,20 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let div = document.querySelector("#container")
-    let ul = document.querySelector("#list")
-    let li = document.querySelector("#item")
-
-    // function logThisAndTarget(event){
-    //     console.log("THIS", this)
-    //     console.log("TARGET", event.target)
-    // } 
-
-    let logThisAndTarget = function(event){
-        console.log("THIS", event.currentTarget)
-        console.log("TARGET", event.target)
-    }
-    div.addEventListener('click', logThisAndTarget)
-    ul.addEventListener('click', logThisAndTarget)
-    li.addEventListener('click', logThisAndTarget)
+    
 })
 
 
--- 34-events-intro/mouse-tracker/mouse_tracker.js
+++ 34-events-intro/mouse-tracker/mouse_tracker.js
@@ -1,10 +1,4 @@
 document.addEventListener("DOMContentLoaded", function(){
 
-    document.addEventListener("mousemove", (e) => {
-        ptag = document.querySelector("#output")
-        ptag.innerText = `X: ${e.clientX}, Y: ${e.clientY}`
-    })
-
-
-
+   
 })
