c746607088ae5b0218a72d112d58b9a0e53e75f0
github username <github email address>
2020-11-24 14:25:33 -0800

    reorganizes and adds comments to 42

--- 42-bookliker-review/js/index.js
+++ 42-bookliker-review/js/index.js
@@ -3,10 +3,34 @@ const userID = 1
 // userID above can be used to set a mock currentUser; for these specs, defaults to 1
 
 document.addEventListener("DOMContentLoaded", function() {
-
+    
+    // set references to DOM Nodes where we'll be injecting our own elements
     const list = document.querySelector("#list")
     const show = document.querySelector('#show-panel')
 
+    // register event listener on above as needed
+    show.addEventListener('click', handleLike)
+    
+    
+    // fetches all books
+    function getBooks(){
+        fetch(baseURL)
+            .then(res => res.json())
+            .then(books => listBooks(books))
+    }
+     
+    // creates <li></li> for each book and adds to DOM
+    function listBooks(books){
+        books.forEach(book => {
+            const item = document.createElement('li')
+            item.dataset.bookId = book.id
+            item.textContent = book.title
+            item.addEventListener('click', showBook)
+            list.appendChild(item)
+        });
+    }
+
+    // renders book detail in show-panel
     function showBook(e){
         getBook(e).then(book => {
             const container = `<div>
@@ -19,39 +43,31 @@ document.addEventListener("DOMContentLoaded", function() {
                 </ul>
             </div>`
             const button = document.createElement('button')
+            // conditional logic checks to see if 'currentUser' is already present in this book's users
             if (book.users.some(user => user.id === userID)){
                 button.innerText = 'UNLIKE'
             } else {
                 button.innerText = "LIKE"
             }
             button.dataset.bookId = book.id
-            // button.setAttribute('name', 'aardvark')
             show.innerHTML = container
             show.appendChild(button)
         })
     }
 
+    // gets a single book by ID
     function getBook(e){
         return fetch(baseURL + `/${e.target.dataset.bookId}`)
                 .then(res => res.json())
     }
-
-    function listBooks(books){
-        books.forEach(book => {
-            const item = document.createElement('li')
-            item.dataset.bookId = book.id
-            item.textContent = book.title
-            item.addEventListener('click', showBook)
-            list.appendChild(item)
-        });
+    
+    // gets a single user by ID
+    function getUser(id){
+        return fetch(`http://localhost:3000/users/${id}`)
+        .then(res => res.json())
     }
 
-    function getBooks(){
-        fetch(baseURL)
-            .then(res => res.json())
-            .then(books => listBooks(books))
-    }
-
+    // executes a PATCH fetch to update likes on a book
     function changeLikes(e, id, body){
         fetch(baseURL + `/${id}`, {
             method: 'PATCH',
@@ -61,26 +77,22 @@ document.addEventListener("DOMContentLoaded", function() {
             },
             body: JSON.stringify(body)
         })
+            // using pessimistic rendering so that list of likers is always derived from the db
             .then(() => showBook(e))
     }
 
-    function getUser(id){
-        return fetch(`http://localhost:3000/users/${id}`)
-        .then(res => res.json())
-    }
-
-    getBooks()
-
-    function handleClick(e){
-        console.dir(e)
+    function handleLike(e){
+        // uses event delegation to detect the origin of a 'click'
         if(e.target.tagName=="BUTTON"){
             const id = e.target.dataset.bookId
+            // conditional to perform different updates dependent on the button text
             if(e.target.innerText == 'LIKE'){
+                // get the "current" user
                 getUser(userID).then(user => {
-
+                    // get the book to update
                     getBook(e).then(book => {
                         const users = book.users
-                        // refactor to fetch user from db
+                        // using the spread operator, append the 'current' user to the list of existing users
                         const body = { users: [...users, user]}
                         changeLikes(e, id, body)
                     })
@@ -88,19 +100,19 @@ document.addEventListener("DOMContentLoaded", function() {
                 
             } else {
                 getUser(userID).then(user => {
-
+                    
                     getBook(e).then(book => {
+                        // similar to above but uses filter to remove 'current' user from the array
                         const users = book.users.filter(user => user.id != userID)
-                        // refactor to fetch user from db
                         const body = { users: [...users]}
                         changeLikes(e, id, body)
                     })
                 })
             }
-
+            
         }
     }
 
-    show.addEventListener('click', handleClick)
+    getBooks()
 
 });
